865b8af5fa8833160343918ba67eedd8e463767f
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-10-15 10:29:24 +0200

    commit before branching

---
+++ src/test/java/app/controller/EventControllerTest.java
@@ -0,0 +1,16 @@
+package app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @Test
+    void getAllevents() {
+    }
+
+    @Test
+    void createNewEvent() {
+    }
+}
